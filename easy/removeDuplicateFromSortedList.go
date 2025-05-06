c76adec9e1f2961e5f34a3e8bd2e735ae01c1013
CaioMicael <contatocaiomicael@gmail.com>
2025-05-05 21:34:39 -0300

    feat - easy: add removeDuplicateFromSortedList

---
+++ easy/removeDuplicateFromSortedList.go
@@ -0,0 +1,25 @@
+package main
+
+// Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	aux := head
+	for aux != nil && aux.Next != nil {
+		if aux.Val == aux.Next.Val {
+			aux.Next = aux.Next.Next
+		} else {
+			aux = aux.Next
+		}
+	}
+	return head
+}
+
+func main() {
+	head := &ListNode{Val: 1}
+	head.Next = &ListNode{Val: 1}
+	deleteDuplicates(head)
+}
